bbd237e4b59336c34e56fcb2bd25ea284971f759
Bullshitman <sokol.joe@gmail.com>
2019-11-05 21:37:32 +0300

    init

---
+++ src/TypeIntro.kt
@@ -0,0 +1,13 @@
+const val PLAYER_NAME: String = "Estragon"
+const val PUBS_NAME: String = "Unicorn's horn"
+const val MAX_EXPIRIENCE:Int = 5000
+val AVAILABLE_STEED: List<String> = listOf("dragon", "minotaur")
+val AVAILABLE_DRINKS: List<String> = listOf("wine", "mead", "lacroix")
+
+fun main(args: Array<String>) {
+    var experiencePoints = 5
+    var availableCoins = 50
+    println(experiencePoints)
+    println(PUBS_NAME)
+    println(PLAYER_NAME.reversed())
+}
