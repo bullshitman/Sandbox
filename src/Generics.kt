afe2d7121cc4b9820ae418cc9317aa116c6bcb07
Bullshitman <sokol.joe@gmail.com>
2019-12-24 22:54:48 +0300

    generics

--- src/Generics.kt
+++ src/Generics.kt
@@ -1,10 +1,19 @@
 class LootBox<T>(item: T) {
+    var open = false
     private var loot: T = item
+    fun fetch(): T? {
+        return loot.takeIf{open}
+    }
 }
 class Fedora(val name: String, val value: Int)
 
 class Coin(val value: Int)
+
+
 fun main(args: Array<String>) {
     val lootBoxOne: LootBox<Fedora> = LootBox(Fedora("a generic-looking fedora", 15))
     val lootBoxTwo: LootBox<Coin> = LootBox(Coin(15))
+    lootBoxOne.fetch()?.run {
+        println("You retrieve $name from the box!")
+    }
 }
