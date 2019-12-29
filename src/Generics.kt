f039fdf50a1eb28678435233bad16fddd0e3a3a9
Bullshitman <sokol.joe@gmail.com>
2019-12-29 23:19:12 +0300

    generics

--- src/Generics.kt
+++ src/Generics.kt
@@ -4,6 +4,9 @@ class LootBox<T>(item: T) {
     fun fetch(): T? {
         return loot.takeIf{open}
     }
+    fun <R> fetch(lootModFunction: (T) -> R):R? {
+        return lootModFunction(loot).takeIf { open }
+    }
 }
 class Fedora(val name: String, val value: Int)
 
@@ -13,8 +16,10 @@ class Coin(val value: Int)
 fun main(args: Array<String>) {
     val lootBoxOne: LootBox<Fedora> = LootBox(Fedora("a generic-looking fedora", 15))
     val lootBoxTwo: LootBox<Coin> = LootBox(Coin(15))
-    lootBoxTwo.open = true
+    lootBoxOne.open = true
     lootBoxOne.fetch()?.run {
         println("You retrieve $name from the box!")
     }
+    val coin = lootBoxOne.fetch { Coin(it.value * 3) }
+    coin?.let { println(it.value) }
 }
