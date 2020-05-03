b79d36d75d4cbc0214aa7c7b96cf9db0e8cb9669
Bullshitman <sokol.joe@gmail.com>
2020-05-03 20:14:03 +0300

    Chapter 18

---
+++ src/Extensions.kt
@@ -0,0 +1,16 @@
+fun String.addEnthusiasm(amount: Int = 1) = this + "!".repeat(amount)
+fun <T> T.easyPrint(): T {
+    println(this)
+    return this
+}
+infix fun String?.printWithDefault(default: String) = print(this ?: default)
+
+val String.numVowels
+    get() = count { "aeiouy".contains(it) }
+fun main(args: Array<String>) {
+    "Madrigal has left the building".easyPrint().addEnthusiasm(9).easyPrint()
+    42.easyPrint()
+    "How many vowels?".numVowels.easyPrint()
+    val nullableString: String? = null
+    nullableString printWithDefault "Default string"
+}
