58502888a3256acdd169e49ac3b2010c951e8879
Bullshitman <sokol.joe@gmail.com>
2020-05-03 19:15:18 +0300

    generics

--- src/Variance.kt
+++ src/Variance.kt
@@ -1,6 +1,8 @@
 class Barrel<out T>(val item: T)
 
-fun main(args: Array<String>){
-    var fedoraBarrel: Barrel<Fedora> = Barrel(Fedora("a great looking fedora", 15))
+fun main(args: Array<String>) {
+    var fedoraBarrel: Barrel<Fedora> = Barrel(Fedora("a generic-looking fedora", 15))
     var lootBarrel: Barrel<Loot> = Barrel(Coin(15))
+    lootBarrel = fedoraBarrel
+    val myFedora: Fedora = lootBarrel.item
 }
